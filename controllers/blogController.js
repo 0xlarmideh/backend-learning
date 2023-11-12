1cbaea4dd4dea2b9aedec3dfe3271c62ba68b906
Abdulhameed <busarioyewole@gmail.com>
2023-11-12 03:00:10 +0100

    Implemented MVC folder structure

---
+++ controllers/blogController.js
@@ -0,0 +1,56 @@
+// 
+const Blog = require("../../models/blog");
+
+const blog_index = (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("blogs/index", { blogs: result, title: "All blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const blog_create_get = (req, res) => {
+  // console.log(req.body);
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+blog_details = (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("blogs/details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const blog_create = (req, res) => {
+  res.render("blogs/create"), { title: "Create a new blog" };
+};
+
+blog_delete = (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {blog_index, blog_create, blog_details, blog_delete}
